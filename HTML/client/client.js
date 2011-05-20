71a355756eef7b096ceb2df9e6d643bcd39e7c98
Florent Jaby <florent.jaby@gmail.com>
2011-05-20 12:45:33 +0200

    allow connection from remote hosts.
    don't hardcode things.

--- HTML/client/client.js
+++ HTML/client/client.js
@@ -14,7 +14,8 @@ var tc = termkit.client = function () {
   this.onDisconnect = function () {};
 
   // Set up socket with back-end.
-  var s = this.socket = new io.Socket('localhost', { port: 2222 }); 
+  //var s = this.socket = new io.Socket('localhost', { port: 2222 }); 
+  var s = this.socket = new io.Socket(); 
   
   // Use shared protocol handler with back-end.
   this.protocol = new termkit.protocol(this.socket, this);
